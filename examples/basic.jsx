b75aceb383e281e01f39efeb52926030da69c517
Georgi Krustev <ggkrustev@yahoo.com>
2016-03-14 19:38:17 +0200

    feat(Animation): Add a basic animation demo
    Show how to animate single children with 'left' transition effect

--- examples/basic.jsx
+++ examples/basic.jsx
@@ -2,7 +2,32 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Animation from '@telerik/kendo-react-animation';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { index: 1 };
+    }
+
+    onClick = () => {
+        this.setState({
+            index: this.state.index + 1
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.onClick}>Left</button>
+                <Animation effect="k-left">
+                    <div className="child-div">{this.state.index}</div>
+                </Animation>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-  <Animation />,
+  <App />,
   document.getElementById('app')
 );
