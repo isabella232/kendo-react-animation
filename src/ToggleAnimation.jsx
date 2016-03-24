41af301feff2fe15f0793af307251aa55a724edd
Georgi Krustev <ggkrustev@yahoo.com>
2016-03-24 17:47:45 +0200

    refactor: Export ToggleAnimation as a React.Component
    This change is required because React cannot apply refs to a pure functions

--- src/ToggleAnimation.jsx
+++ src/ToggleAnimation.jsx
@@ -2,32 +2,32 @@ import React from 'react';
 import Animation from './Animation';
 import styles from '@telerik/kendo-theme-default/styles/animation/main';
 
-const ToggleAnimation = (props) => {
-    const {
-        duration = 300,
-        ...otherProps
-    } = props;
+export default class ToggleAnimation extends React.Component {
+    static propTypes = {
+        children: React.PropTypes.oneOfType([
+            React.PropTypes.element,
+            React.PropTypes.node
+        ]),
+        className: React.PropTypes.string,
+        component: React.PropTypes.string,
+        duration: React.PropTypes.number,
+        fixedContainer: React.PropTypes.bool
+    }
 
-    const transitionGroupProps = {
-        transitionEnterTimeout: duration,
-        transitionLeave: false,
-        transitionName: styles.toggle
-    };
+    render() {
+        const {
+            duration = 300,
+                ...otherProps
+        } = this.props;
 
-    return (
-        <Animation {...transitionGroupProps} {...otherProps} />
-    );
-};
+        const transitionGroupProps = {
+            transitionEnterTimeout: duration,
+            transitionLeave: false,
+            transitionName: styles.toggle
+        };
 
-ToggleAnimation.propTypes = {
-    children: React.PropTypes.oneOfType([
-        React.PropTypes.element,
-        React.PropTypes.node
-    ]),
-    className: React.PropTypes.string,
-    component: React.PropTypes.string,
-    duration: React.PropTypes.number,
-    fixedContainer: React.PropTypes.bool
-};
-
-export default ToggleAnimation;
+        return (
+            <Animation {...transitionGroupProps} {...otherProps} />
+        );
+    }
+}
